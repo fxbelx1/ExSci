774714844b7a0609a6c2df1d6263c5ad32e5c171
Kykal <benavides.alan@hotmail.com>
2020-05-18 08:06:35 -0500

    Fixed references to DB.

--- verificaciondb.php
+++ verificaciondb.php
@@ -1,12 +1,11 @@
 <?php
-    
     require 'conexion.php'; 
     session_start();
 
     $usuario = $_POST['nickname'];
     $contrasena = $_POST['contra'];
 
-    $query = mysqli_query($conexion, " SELECT * FROM usuario WHERE nicknus = '$usuario' and passus = '$contrasena' ");
+    $query = mysqli_query($conexion, " SELECT * FROM usuario WHERE nickus = '$usuario' and passus = '$contrasena' ");
     $nr = mysqli_num_rows($query);
 
     if($nr == 1){
