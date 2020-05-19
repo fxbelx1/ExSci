6b0dbe76e0b6d47c8d5b788b083732c12fd6b927
Kykal <benavides.alan@hotmail.com>
2020-05-18 21:23:27 -0500

    Update.

--- check.php
+++ check.php
@@ -8,36 +8,8 @@
     $r3 = $_POST['p3'];
     $r4 = $_POST['p4'];
 
-    $sql = "SELECT idp, respcorr FROM pregunta";
-    $result = mysqli_query($conexion, $sql);
-    $resultCheck = mysqli_num_rows($result);
+    
 
-    if($resultCheck > 0){
-
-    }else{
-        echo "no";
-    }
-
-    echo $row['idp'];
-
-    if($r1[0] == 'c'){
-        echo "1. Correcta.(".$p1[0].")<br>";
-    }else{
-        echo "1. Incorrecta.<br>";
-    }
-
-    for($i=0; $i < count($r1); $i++){
-        echo "1. Selected " . $r1[$i] . "<br/>";
-    }
-    for($i=0; $i < count($r2); $i++){
-        echo "2. Selected " . $r2[$i] . "<br/>";
-    }
-    for($i=0; $i < count($r3); $i++){
-        echo "3. Selected " . $r3[$i] . "<br/>";
-    }
-    for($i=0; $i < count($r4); $i++){
-        echo "4. Selected " . $r4[$i] . "<br/>";
-    }
 
 
 ?>
--- quimica/nivel1.php
+++ quimica/nivel1.php
@@ -78,38 +78,38 @@
                                 <ol>
                                     <li>
                                         <p>Son las propiedades que varían de forma gradual al movernos en un determinado sentido en el sistema periódico.</p>
-                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p1[]" value="a" id="p1"> <label class="form-check-label" for="exampleRadios1">Propiedades Cualitativas</label>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p1" value="a" id="p1"> <label class="form-check-label" for="exampleRadios1">Propiedades Cualitativas</label>
                                         <br>
-                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p1[]" value="b" id="p1"> <label class="form-check-label" for="exampleRadios2">Propiedades Cuantitativas</label>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p1" value="b" id="p1"> <label class="form-check-label" for="exampleRadios2">Propiedades Cuantitativas</label>
                                         <br>
-                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p1[]" value="c" id="p1"> <label class="form-check-label" for="exampleRadios3">Propiedades Periódicas</label>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p1" value="c" id="p1"> <label class="form-check-label" for="exampleRadios3">Propiedades Periódicas</label>
                                     </li>
                                     <br><br>
                                     <li>
                                         <p>Enlace que resulta de la transferencia de uno o más electrones de un átomo a otro átomo o un grupo de éstos.</p>
-                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p2[]" value="a" id="p2r1"> <label class="form-check-label" for="exampleRadios4">Enlace covalente</label>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p2" value="a" id="p2r1"> <label class="form-check-label" for="exampleRadios4">Enlace covalente</label>
                                         <br>
-                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p2[]" value="b" id="p2r2"> <label class="form-check-label" for="exampleRadios5">Enlace iónico</label>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p2" value="b" id="p2r2"> <label class="form-check-label" for="exampleRadios5">Enlace iónico</label>
                                         <br>
-                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p2[]" value="c" id="p2r3"> <label class="form-check-label" for="exampleRadios6">Enlace metálico</label>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p2" value="c" id="p2r3"> <label class="form-check-label" for="exampleRadios6">Enlace metálico</label>
                                     </li>
                                     <br><br>
                                     <li>
                                         <p>Es un elemento con pocos electrones en su última capa y con gran tendencia a cederlos.</p>
-                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p3[]" value="a" id="p3r1"> <label class="form-check-label" for="exampleRadios7">Metal</label>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p3" value="a" id="p3r1"> <label class="form-check-label" for="exampleRadios7">Metal</label>
                                         <br>
-                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p3[]" value="b" id="p3r2"> <label class="form-check-label" for="exampleRadios8">Semimetal</label>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p3" value="b" id="p3r2"> <label class="form-check-label" for="exampleRadios8">Semimetal</label>
                                         <br>
-                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p3[]" value="c" id="p3r3"> <label class="form-check-label" for="exampleRadios9">No metal</label>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p3" value="c" id="p3r3"> <label class="form-check-label" for="exampleRadios9">No metal</label>
                                     </li>
                                     <br><br>
                                     <li>
                                         <p>¿Cuál es el símbolo del elemento químico Einstenio?</p>
-                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p4[]" value="a" id="p4r1"> <label class="form-check-label" for="exampleRadios10">Ei</label>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p4" value="a" id="p4r1"> <label class="form-check-label" for="exampleRadios10">Ei</label>
                                         <br>
-                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p4[]" value="b" id="p4r2"> <label class="form-check-label" for="exampleRadios11">Es</label>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p4" value="b" id="p4r2"> <label class="form-check-label" for="exampleRadios11">Es</label>
                                         <br>
-                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p4[]" value="c" id="p4r3"> <label class="form-check-label" for="exampleRadios12">No tiene símbolo</label>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;<input class="form-check-input" type="radio" name="p4" value="c" id="p4r3"> <label class="form-check-label" for="exampleRadios12">No tiene símbolo</label>
                                     </li>
                                     <br><br>
                                 </ol>
